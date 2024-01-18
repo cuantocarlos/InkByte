60562cf7087817d240cbc5a622aa769824d4e021
Francisco Carlos Beltran Arsene <fco.carlos@protonmail.com>
2024-01-18 18:57:45 +0100

    creacion validar inicio

--- app/modelo/validar_inicio_sesion.js
+++ app/modelo/validar_inicio_sesion.js
@@ -1,15 +1,14 @@
-import { validarCorreoElectronico } from "../libs/bGeneral.js";
+import { validarCorreoElectronico } from "../libs/bGeneral.js"
 
 
 window.onload = function () {
-    console.log("1.Entra en js validar sesion");
-    let email = document.getElementById("mail");
+    console.log("1.Entra en js validar sesion")
+    let email = document.getElementById("mail")
     email.addEventListener("blur", function (event) {
-        console.log(validarCorreoElectronico(email.value));
+        console.log(validarCorreoElectronico(email.value))
         if (validarCorreoElectronico(email.value) == false) {
-            console.log("2.Entra en if validar sesion ");
+            console.log("2.Entra en if validar sesion ")
             event.preventDefault()
-            
         }
     });
 };
