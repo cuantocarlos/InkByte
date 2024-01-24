5365cd697bcfe309ecd6afdf2fd8c38edefff8cc
Francisco Carlos Beltran Arsene <fco.carlos@protonmail.com>
2024-01-24 21:47:44 +0100

    bases de layout perfil

--- layoutPerfil.php
+++ layoutPerfil.php
@@ -1,49 +1,76 @@
 <?php
-//saco del perfil el nombre y el email
+//variables de sesión
 $nombre = $_SESSION['nombre'];
 $email = $_SESSION['email'];
 $id = $_SESSION['id_user'];
-//una vez ya tengo el nombre y el email, hago la consulta para sacar el resto de datos
-$consulta = "SELECT * FROM usuarios WHERE id_user = '$id'";
+//variables para la base de datos
+$cumple;
+$descripcionProfile;
+$rutaImagenPerfil;
 
+$librosFavoritos;
+$librosLeidos;
+$librosPendientes;
+$librosPublicados;
+//una vez ya tengo el nombre y el email, hago la consulta para sacar el resto de datos
+$consulta = "SELECT * FROM usuarios WHERE id_user = $id";
+$resultado = $conexion->query($consulta);
 ?>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="text-center">Perfil de usuario</h2>
-                    </div>
-                    <div class="card-body">
-                        <div class="text-center mb-4">
-                            <img src="path/to/profile-image.jpg" alt="Profile Image" class="img-fluid rounded-circle"
-                                width="150">
-                        </div>
-                        <h4 class="card-title text-center">John Doe</h4>
-                        <p class="card-text text-center">Web Developer</p>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h5>Contact Information</h5>
-                                <p>Email: john.doe@example.com</p>
-                                <p>Phone: +123 456 7890</p>
-                            </div>
-                            <div class="col-md-6">
-                                <h5>Address</h5>
-                                <p>123 Main Street</p>
-                                <p>Cityville, Country</p>
-                            </div>
-                        </div>
-                        <hr>
-                        <h5>Additional Information</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-                            Sed cursus ante dapibus diam.</p>
-                    </div>
+<div class="container mt-5">
+    <div class="row">
+        <!-- Sección de la foto de perfil -->
+        <div class="col-md-4">
+            <img src="<?php echo $rutaImagenPerfil; ?> " alt="Foto de Perfil" class="img-fluid rounded-circle" />
+        </div>
+
+        <!-- Sección de información del perfil -->
+        <div class="col-md-8">
+            <h2>
+                <?php echo $nombre; ?>
+                <p>
+                    Fecha de Nacimiento:
+                    <?php echo $cumple; ?>
+                </p>
+                <p>
+                    Correo Electrónico:
+                    <?php echo $email; ?>
+                </p>
+                <p>
+                    Descripción:
+                    <?php echo $descripcionProfile; ?>
+                </p>
+            </h2>
+        </div>
+    </div>
+    <!-- Sección de los libros -->
+    <div class="row">
+        <div class="col-md-3">
+            <h3>Libros Favoritos</h3>
+            <!-- <p><?php echo $librosFavoritos; ?></p> -->
+            <!-- Pongo un slider con cards (los libros) -->
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
+        <div class="col-md-3">
+            <h3>Libros Leidos</h3>
+            <p><?php echo $librosLeidos; ?></p>
+        </div>
+        <div class="col-md-3">
+            <h3>Libros Pendientes</h3>
+            <p><?php echo $librosPendientes; ?></p>
+        </div>
+        <div class="col-md-3">
+            <h3>Libros Publicados</h3>
+            <p><?php echo $librosPublicados; ?></p>
+        </div>
     </div>
-
-    <!-- Include Bootstrap JS and Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-csSR1II5TRV6zW2xIqC1l4C6IN5/xtz9F1qQjD8rA7C6MSQ63bAsHtkgqFMybzRc" crossorigin="anonymous"></script>
+</div>
+<!-- Include Bootstrap JS and Popper.js -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-csSR1II5TRV6zW2xIqC1l4C6IN5/xtz9F1qQjD8rA7C6MSQ63bAsHtkgqFMybzRc" crossorigin="anonymous"></script>
