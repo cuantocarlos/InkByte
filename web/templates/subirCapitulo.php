c05a9c7788637734949fd472836f037bccbc9f6a
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2024-01-25 19:54:03 +0100

    Estilo de subirCapitulo

--- web/templates/subirCapitulo.php
+++ web/templates/subirCapitulo.php
@@ -10,35 +10,40 @@ try{
 ?>
 <script type="module" src="../scripts/validar_subirCapitulo.js"></script>
 <div class="container d-flex flex-column mt-5">
-        <h2>Sube tu capítulo</h2>
-        <form action="" method="post" enctype="multipart/form-data">
+          <div class="header p-5 ps-0 pb-4 border-bottom-0">
+            <h1 class="fw-bold mb-0 fs-2 mb-4">Sube tu capítulo</h1>
+          </div>
+          <form action="index.php?ctl=subirCapitulo" method="post" enctype="multipart/form-data">
+                <div class="container d-flex justify-content-end my-2">
+                    <small class="text-body-secondary">Los campos con * son obligatorios</small>
+                </div>
 
-            <div class="form-group">
-                <label for="tus_opciones">Selecciona un libro:</label>
-                <select class="form-control" id="tus_opciones" name="tus_opciones">
-                    <?php
-                        for ($i = 0; $i < count($titulos); $i++) {
-                            echo '<option value='. $ids[$i] . '>' . $titulos[$i] . '</option>';
-                        }                    
-                    ?>
-                </select>
-            </div>
-    
+                <div class="form-group mb-3">
+                    <label for="tus_opciones">Selecciona un libro:</label>
+                    <select class="form-control rounded-3 pt-3 pb-3" id="tus_opciones" name="tus_opciones">
+                        <?php
+                            for ($i = 0; $i < count($titulos); $i++) {
+                                echo '<option value='. $ids[$i] . '>' . $titulos[$i] . '</option>';
+                            }                    
+                        ?>
+                    </select>
+                </div>
 
+                <div class="form-floating mb-3">
+                <input type="text" class="form-control rounded-3" id="titulo_cap" name="titulo_cap" placeholder="Título del capítulo*">
+                <label for="nombre">Título*</label>
+              </div>
 
-            <div class="form-group">
-                <label for="titulo_cap">Título del capítulo:</label>
-                <input type="text" class="form-control" name="titulo_cap" id="titulo_cap">
-            </div>
-            
-            <div class="form-group">
-                <label for="archivoPDF">Selecciona un archivo PDF:</label>
-                <input type="file" class="form-control-file" id="archivoPDF" name="archivoPDF" accept=".pdf">
-            </div>
+              <div class="mb-5">
+              <label for="f_perfil" class="form-label">Seleccioine una foto de perfil:</label>
+              <input class="form-control" type="file" id="archivoPDF" name="archivoPDF" accept=".pdf">
+              </div>
 
-            <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary mt-4" type="submit" name="bAceptar">Subir capítulo</button>
-        </form>
+                <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary mt-4" type="submit" name="bAceptar">Subir capítulo</button>
+            </form>
+
     </div>
 
 
 
+<?php include('layout.php'); ?>
