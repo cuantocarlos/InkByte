5daf33ac050fa85dde85565bd75d9b003b2b4c6c
cristian462 <131487492+cristian462@users.noreply.github.com>
2024-01-25 15:57:24 +0100

    añadido template de layout

--- web/templates/inicio.php
+++ web/templates/inicio.php
@@ -1,23 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-
-    <link rel="stylesheet" href="../css/styles/navbar.css">
-</head>
-
-<body>
-
-    <div class="superNav border-bottom py-2 bg-light">
+  <div class="superNav border-bottom py-2 bg-light">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 centerOnMobile">
@@ -98,6 +79,4 @@
 
 </html>
 
-</body>
-
-</html>
+<?php include('layout.php'); ?>
