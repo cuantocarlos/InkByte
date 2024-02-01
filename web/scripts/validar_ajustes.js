08eb0b11bb523059d4b87c5cf603401d6bc9ae39
Francisco Carlos Beltran Arsene <fco.carlos@proton.me>
2024-02-01 17:07:47 +0100

    variables a validar

---
+++ web/scripts/validar_ajustes.js
@@ -0,0 +1,18 @@
+import{}from"./scripts/bGeneral.js"
+var nombre = document.getElementById("nombre");
+var nick = document.getElementById("nick");
+var email = document.getElementById("mail");
+var oldpass = document.getElementById("oldpass");
+var pass = document.getElementById("pass");
+var pass2 = document.getElementById("pass2");
+var fecha = document.getElementById("fecha");
+var descripcion = document.getElementById("descripcion");
+var rol = document.getElementsByName("options-base");
+
+
+
+window.onload = function () {
+//cojo lo que hay en la base de datos y lo pongo en los campos
+
+}
+
--- web/templates/perfil_ajustes.php
+++ web/templates/perfil_ajustes.php
@@ -3,37 +3,37 @@
 
 <div class="container">
     <div class="header p-5 pb-4 border-bottom-0">
-        <h1 class="fw-bold mb-0 fs-2 mb-4">Modifica los datos</h1>
+        <h1 class="fw-bold mb-0 fs-2 mb-4">Modifica los datos que desees cambiar</h1>
     </div>
     <div class="body p-5 pt-0">
         <form action="index.php?ctl=registro" method="post" enctype="multipart/form-data">
             <div class="form-floating mb-3">
-                <input type="text" class="form-control rounded-3" id="nick" name="nick" placeholder="nick" pattern="[A-Za-z\sñÑçÇ]+" minlength="3" maxlength="60"/>
+                <input type="text" class="form-control rounded-3" id="nombre" name="nombre" placeholder="nombre" pattern="[A-Za-z\sñÑçÇ]+" minlength="3" maxlength="60" />
                 <label for="nombre">Nombre y Apellidos</label>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">@</span>
                 <div class="form-floating">
-                    <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre" pattern="[A-Za-z\sñÑçÇ]+"/>
-                    <label for="nombre">Nombre de Usuario</label>
+                    <input type="text" class="form-control" id="nick" placeholder="nick" name="nick" pattern="[A-Za-z\sñÑçÇ]+" />
+                    <label for="nick">Nombre de Usuario</label>
                 </div>
             </div>
             <div class="form-floating mb-3">
-                <input type="email" class="form-control rounded-3" id="mail" placeholder="name@example.com" name="mail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+                <input type="email" class="form-control rounded-3" id="mail" placeholder="name@example.com" name="mail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
                 <label for="mail">Correo Electrónico</label>
             </div>
             <div class="form-floating mb-3">
                 <input type="password" class="form-control rounded-3" id="oldpass" placeholder="Password" name="oldpass" />
-                <label for="oldpass">Contraseña Anterior</label>
+                <label for="oldpass">Contraseña Actual</label>
             </div>
             <small class="text-body-secondary mb-1">Mínimo: 8 caracteres, mayúsculas, minúsculas, y caracteres.</small>
             <div class="form-floating mb-3">
-                <input type="password" class="form-control rounded-3" id="pass" placeholder="Password" name="pass" pattern="(?=.*\d)(?=.*[a-zñç])(?=.*[A-ZÇÑ])(?=.*[$@¿?¡!_-])[a-zA-Z\d$@¿?¡!_-ñÑçÇ]{8,16}"/>
-                <label for="pass">Nueva Contraseña</label>
+                <input type="password" class="form-control rounded-3" id="pass" placeholder="Password" name="pass" pattern="(?=.*\d)(?=.*[a-zñç])(?=.*[A-ZÇÑ])(?=.*[$@¿?¡!_-])[a-zA-Z\d$@¿?¡!_-ñÑçÇ]{8,16}" />
+                <label for="pass">Contraseña Nueva</label>
             </div>
             <div class="form-floating mb-3">
                 <input type="password" class="form-control rounded-3" id="pass2" placeholder="Password" name="pass2" />
-                <label for="pass2">Repite la Contraseña Nueva</label>
+                <label for="pass2">Repetir Contraseña Nueva</label>
             </div>
             <div class="form-floating mb-3">
                 <input type="date" class="form-control rounded-3" id="fecha" name="fecha" />
@@ -41,7 +41,7 @@
             </div>
             <div class="mb-5">
                 <label for="f_perfil" class="form-label">Para modificar su foto de perfil seleccione la nueva:</label>
-                <input class="form-control" type="file" id="f_perfil" name="f_perfil" />
+                <input class="form-control" type="file" id="f_perfil" name="f_perfil" accept="image/<?php $extensionesValidas?>" />
             </div>
             <small class="text-body-secondary mb-1">Máximo 300 caracteres.</small>
             <div class="form-floating">
@@ -62,3 +62,4 @@
 </div>
 
 <?php include 'layout.php'?>
+<!--Poner el $extensionesValidas en el input de la foto de perfil-->
