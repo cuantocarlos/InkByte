b2b36dfd59f16a8478de9519b6884132fe31703c
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2024-02-06 23:54:02 +0100

    layout-terminado

--- web/templates/recomendaciones.php
+++ web/templates/recomendaciones.php
@@ -1,16 +1,21 @@
-<div class="container border bg-rosa" style="padding-top:5rem;">
+<div class="container bg-rosa" style="padding-top:5rem;">
     <h1 class="mb-4">Recomendaciones</h1>
     <?php
 if ($params["num_generos"] > 0) {
     foreach ($params["recomendaciones"] as $genero => $libros) {
-        echo '<div class="genero border d-flex flex-column align-items-center">';
-        echo '    <h3>' . ucfirst($genero) . '</h3>';
-        for ($i = 0; $i < 1; $i++) {
-            echo '    <div class="libro border d-flex flex-column ">';
-            echo '        <img src="../app/archivos/img/libro/' . $libros[$i]['imagen_portada'] . '" class="flex-shrink-0" style="height: 150px;">';
-            echo '        <p>'. $libros[$i]['titulo'] .'</p>';
+        echo '<div class="genero border-bottom d-flex flex-column  justify-content-center align-items-center">';
+        echo '    <div class="align-self-start"><h3>' . ucfirst($genero) . '</h3></div>';
+        echo '<div class="d-flex flex-row align-items-center">';
+        for ($i = 0; $i < count($params["recomendaciones"][$genero]); $i++) {
+            echo '<a href="index.php?ctl=book&id_libro=' . $libros[$i]['id_libro'] . '" class="link-div">';
+            echo '    <div class="m-3">';
+            echo '        <div class="d-flex justify-content-center"><img src="../app/archivos/img/libro/' . $libros[$i]['imagen_portada'] . '" class="flex-shrink-0" style="height: 150px; flex-shrink: 0;"></div>';
+            echo '        <p>' . $libros[$i]['titulo'] . '</p>';
             echo '    </div>';
+            echo '</a>';
         }
+        
+        echo '</div>';
         echo '</div>';
     }
 } else {
